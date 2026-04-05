72da875067dbacb804bc200ac62dcd5f3385083f
Lakshya115-spec <lakshyaadhhi12@gmail.com>
2026-04-05 16:42:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+connectDB();
+app.use(express.json());
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/bank", require("./routes/bankRoutes"));
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
